0f2db54591d3eb3775a4c9982745a70795a83886
Cristian <ioancristian1207@gmail.com>
2019-06-01 16:46:14 +0300

    rezolvare metode hasItem(), clear()

--- Inventory.java
+++ Inventory.java
@@ -1,34 +1,44 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Inventory <T> {
+public class Inventory<T> {
 
-    private Map<T,Integer> inventory;
+    private Map<T, Integer> inventory;
 
-    public Inventory(){
+    public Inventory() {
         inventory = new HashMap<>();
     }
 
     //int getQuantity(T item) - returns quantity for the given item;
 
-    public int getQuantity (T Item){
-        if (!inventory.isEmpty()){
-            System.out.println(" Stoc existent: " + inventory.get(getQuantity(Item)));
+    public int getQuantity(T Item) {
+        int quantity = getQuantity(Item);
+        if (!inventory.isEmpty()) {
+            System.out.println(" Stoc existent: " + inventory.get(quantity));
         }
-        int quantity = 0;
         return quantity;
     }
 
-    //public void add(T Item){};
+    public void add(T Item) {
+
+    }
 
 
-    //public boolean hasItem(Item){};
+    public boolean hasItem(T Item) {
+        if (inventory.containsKey(Item)) return true;
+        else return false;
+    }
 
-    //public void decrease (T, item){};
+    public void decrease (T Item){
+        this.decrease(Item);
+    }
 
-    //public void clear (){};
+    public void clear () {
+        inventory.clear();
+        System.out.println("Finally the inventory look like this: " + inventory);
+    }
 
-    //public void put(T item, int quantity){};
+    public void put(T item, int quantity){
 
-
+    }
 }
